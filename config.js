6153bbcb76cf89bbe09b469b3d48d2f91b770e03
vutivisithole <87698452+vutivisithole@users.noreply.github.com>
2022-06-28 08:43:01 +0200

    Module-12-Assessment-1

---
+++ config.js
@@ -0,0 +1,11 @@
+
+
+let rootpath = "https://mysite.itvarsity.org/api/ContactBook/";
+let apiKey = checkApiKey();
+
+function checkApiKey(){
+    if (!localStorage.getItem("apiKey")) {
+        window.open("enter-api-key.html", "_self");
+    }
+    return localStorage.getItem("apiKey");
+}
